e1f82ae039903616a89cfb7fe03e1d967963cfce
visualstudio <visualstudio@230-PC45>
2019-03-14 16:19:24 -0400

    commit6

---
+++ UnitTestProject1/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UnitTestProject1
+{
+    internal class BankAccount
+    {
+        private string v;
+        private double beginningBalance;
+
+        public BankAccount(string v, double beginningBalance)
+        {
+            this.v = v;
+            this.beginningBalance = beginningBalance;
+        }
+
+        internal void Debit(double debitAmount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -8,7 +8,19 @@ namespace UnitTestProject1
     {
         [TestMethod]
         public void TestMethod1()
+        { }
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+
+
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
         {
+            double beginningBalance = 11.99;
+            double debitAmount = -100.00;
+            BankAccount account = new BankAccount("Mr.Bryan Walton", beginningBalance);
+
+            //act
+            account.Debit(debitAmount);
         }
+
     }
 }
